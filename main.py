c7c1e94009a60823ba8468a5c8ecf21a6e0c91b9
gg12 darfren <frenkiarts@yandex.ru>
2024-12-01 21:39:20 +0400

    мб сделал фазу 1

--- main.py
+++ main.py
@@ -2,4 +2,5 @@ from simplex_table import SimplexTable
 
 st = SimplexTable(3, 2, [-1, -4, -1], [[1, -1, 1], [2, -5, -1]], [3, 0])
 # st = SimplexTable(4, 3, [1, -2, 3, -10], [[1, 1, 2, -6], [1, 1, 4, -8], [4, 2, 1, -4]], [1, 1, 3])
+st.phase1()
 st.print_table_info()
--- simplex_table.py
+++ simplex_table.py
@@ -1,6 +1,3 @@
-import numpy as np
-
-
 class SimplexTable:
 
     def __init__(self, n, m, f, a, b):
@@ -15,16 +12,16 @@ class SimplexTable:
         
         for i in range(m):
             self.a[n + i][i] = 1
-    #элемент базиса храню как тапл вида (номер переменной, значение, домноженно ли до m)
+    #элемент базиса храню как тапл вида (номер переменной, значение, домноженно ли до M)
         self.basis = [(n + i, 1, 1) for i in range(m)]
-    #схожим образом храню коэффициенты в целевой функции
+    #схожим образом храню коэффициенты в целевой функции (значеник, домноженно ли на M)
         self.f = [(f[i], 0) for i in range(n)]
         self.f += [(1, 1) for i in range(m)]
     #значения моих оценок для каждого столбца с m и без нее
         self.score = [0 for i in range(n + m)]
         self.score_m = [0 for i in range(n + m)]
     #текущие значения правых сторон ограничений
-        self.a0 = b
+        self.b = b
         self._update_score()
     
     def _update_score(self): #функция для пересчета оценок для столбцов
@@ -34,7 +31,6 @@ class SimplexTable:
         for i in range(self.n + self.m):
             for j in range(self.m):
                 res = self.a[i][j] * self.basis[j][1]
-                print(res)
                 if self.basis[j][2] == 0:
                     self.score[i] += res
                 else:
@@ -44,11 +40,50 @@ class SimplexTable:
             else:
                 self.score_m[i] -= self.f[i][0]
 
+    def _check_is_optimal_phase1(self): #функция для проверки того, что фаза 1 закончилась
+        for i in self.score_m:
+            if self.score_m > 0:
+                return False
+        return True
+
+    def _relax_table(self, sz, f): #функция для ввода вектора, соответствующего максимуму, в базис
+        current_max = 0
+        current_min = 0
+        max_ind = -1
+        min_ind = -1
+        for i in range(sz):
+            if f[i] > current_max:
+                max_ind = i
+                current_max = f[i]
+        for i in range(len(self.a[max_ind])):
+            if self.b[i] / self.a[max_ind][i] < current_min or min_ind == -1:
+                current_min = self.b[i] / self.a[max_ind][i]
+                min_ind = i
+        for i in range(self.m): #релаксируем столбец b
+            if i != min_ind:
+                self.b[i] = self.b[i] - (self.b[min_ind] / self.a[max_ind][min_ind]) * self.a[max_ind][i]
+            else:
+                self.b[i] = self.b[min_ind] / self.a[max_ind][min_ind]
+        tmp = [[0 for j in range(len(self.a[i]))] for i in range(len(self.a))]
+        for i in range(sz): #релаксируем матрицу
+            for j in range(len(self.a[i])):
+                if j == min_ind:
+                    tmp[i][j] = self.a[i][j] / self.a[max_ind][min_ind]
+                else:
+                    tmp[i][j] = self.a[i][j] - self.a[i][min_ind] / self.a[max_ind][min_ind] * self.a[max_ind][j]
+        self.a = tmp
+        self.basis[min_ind] = (max_ind, *self.f[max_ind])
+        self._update_score()
+
+    def phase1(self):
+        # while self.check_is_optimal_phase1():
+            self._relax_table(self.n + self.m, self.score_m)
+
     def print_table_info(self):
         for i in range(self.n + self.m):
             print(*self.a[i])
         print(*self.basis)
         print(*self.f)
-        print(*self.a0)
+        print(*self.b)
         print(*self.score)
         print(*self.score_m)
